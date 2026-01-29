48706da383d3a1f2a814a9b913f3aebbc5e003b4
«Trc300online» <trieracolomar@gmail.com>
2026-01-29 13:46:48 +0100

    done act3 (6)

--- KeySwapp/customTailwind/src/App.jsx
+++ KeySwapp/customTailwind/src/App.jsx
@@ -51,6 +51,13 @@ function App() {
       <hr className='hr'/>
       <hr className='hr'/>
       <hr className='hr'/>
+
+      <button className='btn'>asdf</button>
+      <button className='btn-primary'>asdf</button>
+      <button className='btn-secondary'>asdf</button>
+      <p className='badge'>asdffasdasdfasfasff</p>
+      <a href="#" className='nav-link'>asdfsadf</a>
+      <a href="#" className='nav-link-active'>asdfsadf</a>
       <ul className='ul'>
         <li>asdf</li>
         <li>asdf</li>
@@ -65,6 +72,11 @@ function App() {
       </ol>
 
       <p className='alert'>asdfhklasjdfhlsjkadfhaskjdfhaskljfhaskjldfhsaf</p>
+      <p className='alert-info'>asdfhklasjdfhlsjkadfhaskjdfhaskljfhaskjldfhsaf</p>
+      <p className='alert-warning'>asdfhklasjdfhlsjkadfhaskjdfhaskljfhaskjldfhsaf</p>
+      <p className='alert-error'>asdfhklasjdfhlsjkadfhaskjdfhaskljfhaskjldfhsaf</p>      
+      <p className='alert-success'>asdfhklasjdfhlsjkadfhaskjdfhaskljfhaskjldfhsaf</p>
+      <p className='card'>asdfhklasjdfhlsjkadfhaskjdfhaskljfhaskjldfhsaf</p>
       <p className='p'>Comença a crear la teva pàgina de documentació aquí! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit quas sapiente perferendis hic voluptates culpa quibusdam. Vel unde, nisi officiis dignissimos mollitia repudiandae autem magnam. Error fugit minus rerum obcaecati.</p>
       <p className='p'>Comença a crear la teva pàgina de documentació aquí! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, laudantium fugiat at aspernatur explicabo a ut, nam pariatur repellat inventore perspiciatis voluptates aut dolores delectus esse sunt ducimus veniam harum.</p>
     </div>
